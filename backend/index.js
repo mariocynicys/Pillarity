b18322732e0d33fafc72127d9a00b62a10e34930
meryacine <omar.groza@gmail.com>
2021-02-22 12:08:19 +0200

    boiler

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { MongoClient } from 'mongodb';
+
+const uri = "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb";
+const db = new MongoClient(uri);
+
+const typeDefs = gql`
+  type Query {
+    hello: String,
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'This Is Mind Blowing',
+  },
+};
+ 
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+ });
+
+ 
+const app = express();
+
+server.applyMiddleware({ app });
+ 
+app.listen({ port: 4000 }, () =>
+  console.log('Now browse to http://localhost:4000' + server.graphqlPath)
+);
